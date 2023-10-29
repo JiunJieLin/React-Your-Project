01f656fe10cc27b6a278e3e006a1f734968c558e
JiunJieLin <sl10066116@gmail.com>
2023-10-29 18:13:51 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+const Home = () => {
+  return (
+    <div className="  mx-auto min-h-screen max-w-[1920px] bg-white">
+      <div className="relative h-[1000px] w-full ">
+        <div className=" absolute bottom-0 h-[900px] w-[500px] rounded-tr-3xl bg-black"></div>
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
